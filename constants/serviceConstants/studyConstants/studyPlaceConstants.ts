4e202c304813164c87a534933962cb6cc1e9a302
LSJ <j44s11@naver.com>
2024-06-07 16:12:09 +0900

    add study

--- constants/serviceConstants/studyConstants/studyPlaceConstants.ts
+++ constants/serviceConstants/studyConstants/studyPlaceConstants.ts
@@ -2,84 +2,81 @@
 export const ALL_스터디인증 = "6479f39e907daf3e8a5cda6e";
 
 //수원
-export const SUWAN_수원시청 = "64395f1e8d1edf4e056e993d";
-export const SUWAN_수원역 = "6637352830c7250eac355cc6";
 export const SUWAN_아주대 = "62e5d6e0e098c232c05c3d3f";
-export const SUWAN_상현역 = "64b15fb72704a3af110733ba";
+export const SUWAN_수원시청 = "64395f1e8d1edf4e056e993d";
 export const SUWAN_송죽 = "644f6ffe9edb56992ccda5e9";
-export const SUWAN_경희대 = "661cdae4aaae875d410236d2";
+export const SUWAN_상현역 = "64b15fb72704a3af110733ba";
+export const SUWAN_광교역 = "65211465032de083c092353c";
 export const SUWAN_구운동 = "6527c32774e4622ec48d12dd";
-export const SUWAN_스타벅스 = "64ba45feefd3711264521a87";
-export const SUWAN_경기대 = "65211465032de083c092353c";
 export const SUWAN_고색역 = "654ca7770f651095a2e41845";
 export const SUWAN_성균관대역 = "654ca7b50f651095a2e41846";
 export const SUWAN_광교엘리웨이 = "654ca92f0f651095a2e41848";
-export const SUWAN_워터쿨러 = "65bfc9347d1dd938049ed1a2";
 export const SUWAN_망포역 = "6559f61fe5c9066b9b8e7d29";
 export const SUWAN_행궁동 = "6559f7e8e5c9066b9b8e7d2a";
+export const SUWAN_경기대 = "65bfc9347d1dd938049ed1a2";
 export const SUWAN_광교 = "65fbdd5eba2597c46415a02c";
+export const SUWAN_경희대 = "661cdae4aaae875d410236d2";
+export const SUWAN_수원역 = "6637352830c7250eac355cc6";
 
 //양천
-export const YANG_위카페 = "6437e9d7ab537b843c6143ac";
-export const YANG_할리스 = "6437ea69ab537b843c6143ad";
-export const YANG_파스쿠찌 = "652a5093db9e8e554deafd63";
-export const YANG_이디야 = "6456f0b320352b2d8758295c";
-export const YANG_카페베네 = "64ba43a5efd3711264521a86";
-export const YANG_스타벅스 = "64bbc30787f8dc1a5210da6a";
-export const YANG_카페꼼마 = "650afdb79ea186d7d9b2a1b6";
-export const YANG_몽글 = "650afd559ea186d7d9b2a1b5";
-export const YANG_신풍역 = "6551d99dd01680084c5c6a92";
-export const YANG_신길역 = "6551d7cbd01680084c5c6a91";
-export const YANG_신월동 = "6551d5bdd01680084c5c6a90";
+export const YANG_등촌역 = "6437e9d7ab537b843c6143ac";
+export const YANG_당산역 = "6437ea69ab537b843c6143ad";
+export const YANG_오목교역 = "6456f0b320352b2d8758295c";
+export const YANG_영등포구청역 = "64ba43a5efd3711264521a86";
+export const YANG_화곡DT점 = "64bbc30787f8dc1a5210da6a";
+export const YANG_신도림역 = "650afd559ea186d7d9b2a1b5";
+export const YANG_여의도역 = "650afdb79ea186d7d9b2a1b6";
+export const YANG_까치산역 = "652a5093db9e8e554deafd63";
 export const YANG_양천구청역 = "6551d2b3d01680084c5c6a8f";
+export const YANG_신월동점 = "6551d5bdd01680084c5c6a90";
+export const YANG_신길역 = "6551d7cbd01680084c5c6a91";
+export const YANG_신풍역 = "6551d99dd01680084c5c6a92";
 export const YANG_화곡역 = "658fbbc2113d3959183e36c2";
-export const YANG_발산역 = "65bfac0a7d1dd938049ed19e";
 export const YANG_목동점 = "65bfab8b7d1dd938049ed19d";
-export const YANG_당산역 = "661cdb45aaae875d410236d3";
+export const YANG_발산역 = "65bfac0a7d1dd938049ed19e";
+export const YANG_선유도점 = "661cdb45aaae875d410236d3";
 
 //안양
-export const ANYANG_커피인더스트리 = "6456ef2820352b2d87582959";
-export const ANYANG_숨맑은집 = "6456ede120352b2d87582955";
-export const ANYANG_파스쿠찌 = "652a52a6db9e8e554deafd67";
-export const ANYANG_인뎃커피 = "6456ef8120352b2d8758295b";
-export const ANYANG_범계학원가 = "6551ba13d01680084c5c6a8c";
-export const ANYANG_호계 = "6551bdbfd01680084c5c6a8d";
+export const ANYANG_범계역 = "6456ede120352b2d87582955";
+export const ANYANG_금정역 = "6456ef2820352b2d87582959";
+export const ANYANG_인덕원역 = "6456ef8120352b2d8758295b";
+export const ANYANG_안양역 = "652a52a6db9e8e554deafd67";
+export const ANYANG_범계학원가점 = "6551ba13d01680084c5c6a8c";
+export const ANYANG_호계점 = "6551bdbfd01680084c5c6a8d";
 
 //강남
-export const GANGNAM_강남 = "652a9e8adb9e8e554deafd68";
-export const GANGNAM_신논현 = "64ecc33940f6dace3b065598";
-export const GANGNAM_논현 = "64ecc3c240f6dace3b06559c";
-export const GANGNAM_교대 = "652a5245db9e8e554deafd66";
-export const GANGNAM_양재 = "64ecc48d40f6dace3b06559e";
-export const GANGNAM_선릉 = "64ecc54240f6dace3b0655a0";
-export const GANGNAM_강남구청 = "64ecc4ef40f6dace3b06559f";
-export const GANGNAM_도곡 = "65522b7ed01680084c5c6a93";
+export const GANGNAM_신논현역 = "64ecc33940f6dace3b065598";
+export const GANGNAM_논현역 = "64ecc3c240f6dace3b06559c";
+export const GANGNAM_강남구청역 = "64ecc4ef40f6dace3b06559f";
+export const GANGNAM_선릉역 = "64ecc54240f6dace3b0655a0";
+export const GANGNAM_교대역 = "652a5245db9e8e554deafd66";
+export const GANGNAM_강남역 = "652a9e8adb9e8e554deafd68";
+export const GANGNAM_도곡동점 = "65522b7ed01680084c5c6a93";
 
 //동대문
-export const DONG_장안동 = "6597c1f1a11634850ed506e0";
-export const DONG_석계역 = "6597c184a11634850ed506df";
-export const DONG_딥십리역 = "6597c124a11634850ed506de";
-export const DONG_서울시립대 = "6597c0a2a11634850ed506dd";
-export const DONG_경희대 = "661cd9aeaaae875d410236d1";
-export const DONG_월곡역 = "6597c047a11634850ed506db";
-export const DONG_왕십리역 = "6597c004a11634850ed506da";
-export const DONG_안암역 = "6597bfd5a11634850ed506d9";
-export const DONG_신설동역 = "6597bf9ca11634850ed506d8";
-export const DONG_길음역 = "6597bf65a11634850ed506d7";
-export const DONG_성신여대 = "6597bf26a11634850ed506d6";
-export const DONG_동대문역 = "6597bed1a11634850ed506d5";
 export const DONG_혜화역 = "6597be44a11634850ed506d3";
-export const DONG_혜화역2 = "6597be67a11634850ed506d4";
+export const DONG_동대문역 = "6597bed1a11634850ed506d5";
+export const DONG_성신여대점 = "6597bf26a11634850ed506d6";
+export const DONG_길음역 = "6597bf65a11634850ed506d7";
+export const DONG_신설동역 = "6597bf9ca11634850ed506d8";
+export const DONG_안암역 = "6597bfd5a11634850ed506d9";
+export const DONG_왕십리역 = "6597c004a11634850ed506da";
+export const DONG_월곡역 = "6597c047a11634850ed506db";
+export const DONG_서울시립대점 = "6597c0a2a11634850ed506dd";
+export const DONG_딥십리역 = "6597c124a11634850ed506de";
+export const DONG_석계역 = "6597c184a11634850ed506df";
+export const DONG_장안동사거리점 = "6597c1f1a11634850ed506e0";
 export const DONG_화랑대역 = "65bfac8f7d1dd938049ed19f";
 export const DONG_중화역 = "6604c1ee4920fb76c1a64e62";
+export const DONG_회기역 = "661cd8d3aaae875d410236d0";
+export const DONG_경희대점 = "661cd9aeaaae875d410236d1";
 export const DONG_노원역 = "663739e230c7250eac355cc8";
 export const DONG_수유역 = "66373a7d30c7250eac355cc9";
-export const DONG_회기역 = "661cd8d3aaae875d410236d0";
 
 //인천
-export const INC_인하대 = "65fbc711ba2597c46415a027";
-export const INC_송도DT = "65fbc8eeba2597c46415a028";
-export const INC_송도 = "65fbcba6ba2597c46415a029";
-export const INC_구월동 = "65fbd787ba2597c46415a02a";
-export const INC_부평 = "65fbd888ba2597c46415a02b";
-export const INC_인천대 = "6637373030c7250eac355cc7";
+export const INC_인하대점 = "65fbc711ba2597c46415a027";
+export const INC_송도DT점 = "65fbc8eeba2597c46415a028";
+export const INC_송도점 = "65fbcba6ba2597c46415a029";
+export const INC_구월동점 = "65fbd787ba2597c46415a02a";
+export const INC_부평점 = "65fbd888ba2597c46415a02b";
+export const INC_인천대점 = "6637373030c7250eac355cc7";
