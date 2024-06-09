724f61f028bf86440200dbeff22f294c8c1a770c
SeungJu, Lee <84257439+SeungJL@users.noreply.github.com>
2024-06-09 16:15:14 +0900

    fix location cache issue #161 (#162)

--- pageTemplates/study/StudyHeader.tsx
+++ pageTemplates/study/StudyHeader.tsx
@@ -1,11 +1,13 @@
 import dayjs from "dayjs";
 import { useParams } from "next/navigation";
 import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
 import KakaoShareBtn from "../../components/atoms/Icons/KakaoShareBtn";
 import Header from "../../components/layouts/Header";
 import { PLACE_TO_LOCATION } from "../../constants/serviceConstants/studyConstants/studyLocationConstants";
 import { WEB_URL } from "../../constants/system";
+import { convertLocationLangTo } from "../../utils/convertUtils/convertDatas";
 import { dayjsToFormat } from "../../utils/dateTimeUtils";
 interface IStudyHeader {
   brand: string;
@@ -15,14 +17,16 @@ interface IStudyHeader {
 }
 
 function StudyHeader({ brand, fullname, locationDetail, coverImage }: IStudyHeader) {
+  const { data: session } = useSession();
   const { id, date } = useParams<{ id: string; date: string }>();
-  const location = PLACE_TO_LOCATION?.[id];
+  const location = PLACE_TO_LOCATION?.[id] || session?.user.location;
+  const locationEn = convertLocationLangTo(location, "en");
   const router = useRouter();
 
   const url = WEB_URL + router?.asPath;
 
   return (
-    <Header title={brand} url={`/home/?location=${location}&date=${date}`}>
+    <Header title={brand} url={`/home/?location=${locationEn}&date=${date}`}>
       {brand !== "자유 신청" && (
         <KakaoShareBtn
           type="study"
