95d37869655dc42a5ff7d772f7835fcca12c926c
SeungJu, Lee <84257439+SeungJL@users.noreply.github.com>
2024-06-11 22:01:50 +0900

    UI upgrade (#164)

--- pageTemplates/home/HomeStudySection.tsx
+++ pageTemplates/home/HomeStudySection.tsx
@@ -1,8 +1,8 @@
 import { ThemeTypings } from "@chakra-ui/react";
 import dayjs from "dayjs";
 import { AnimatePresence, motion, PanInfo } from "framer-motion";
-import { useRouter, useSearchParams } from "next/navigation";
 import { useSession } from "next-auth/react";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import styled from "styled-components";
