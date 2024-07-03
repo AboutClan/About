d2d4bb14eb65e56b750cfc7fcb37ea38eb17b968
createhb21 <alsghk9701@gmail.com>
2024-07-03 18:11:08 +0900

    pwa bug debugging, wip

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -1,4 +1,3 @@
-/* eslint-env serviceworker */
 /* global workbox */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js");
 // This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
@@ -7,40 +6,24 @@ workbox.setConfig({
   debug: false,
 });
 
-const CACHE = "pwabuilder-offline-page";
-const API_CACHE = "api-cache";
-const STATIC_RESOURCES = "static-resources";
-const IMAGES_CACHE = "images";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "/offline";
+const offlinePageCache = "pwa-offline-page-cache";
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches
-      .open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-      .then(() => {
-        return self.skipWaiting();
-      }),
-  );
+self.addEventListener("install", async (event) => {
+  event.waitUntil(caches.open(offlinePageCache).then((cache) => cache.add(offlineFallbackPage)));
 });
 
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [CACHE, API_CACHE, STATIC_RESOURCES, IMAGES_CACHE];
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return Promise.all(
-          cacheNames.map((cacheName) => {
-            if (!cacheWhitelist.includes(cacheName)) {
-              return caches.delete(cacheName);
-            }
-          }),
-        );
-      })
-      .then(() => self.clients.claim()),
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode !== "navigate") {
+    // page navigation 제외
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(async () => {
+      const cache = await caches.open(CACHE_NAME);
+      return await cache.match("offline");
+    }),
   );
 });
 
@@ -49,6 +32,7 @@ self.addEventListener("message", (event) => {
     self.skipWaiting();
   }
 });
+
 self.addEventListener("push", (e) => {
   const data = e.data.json();
 
@@ -73,67 +57,6 @@ self.addEventListener("push", (e) => {
     dir: data?.dir,
   });
 });
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-} // Adjust caching strategy for API requests
-workbox.routing.registerRoute(
-  new RegExp(`${SERVER_URI}/.*`), // Adjust the regex to match your API requests
-  new workbox.strategies.NetworkFirst({
-    cacheName: API_CACHE,
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 50,
-        maxAgeSeconds: 24 * 60 * 60, // 1 day
-      }),
-    ],
-    fetchOptions: {
-      cache: "no-store",
-    },
-  }),
-);
-self.addEventListener("install", async (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage)));
-});
-
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-workbox.routing.registerRoute(
-  new RegExp("/_next/static/.*"),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: STATIC_RESOURCES,
-  }),
-); // Adjust caching strategy for images
-workbox.routing.registerRoute(
-  new RegExp("/_next/image.*"),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: IMAGES_CACHE,
-  }),
-);
-
-// Fallback for navigation requests
-self.addEventListener("fetch", (event) => {
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      (async () => {
-        try {
-          const preloadResp = await event.preloadResponse;
-          if (preloadResp) {
-            return preloadResp;
-          }
-
-          const networkResp = await fetch(event.request);
-          return networkResp;
-        } catch (error) {
-          const cache = await caches.open(CACHE);
-          const cachedResp = await cache.match(offlineFallbackPage);
-          return cachedResp;
-        }
-      })(),
-    );
-  }
-});
 
 // Notification click event
 self.addEventListener("notificationclick", (event) => {
@@ -157,3 +80,108 @@ self.addEventListener("notificationclick", (event) => {
     }),
   );
 });
+
+// ** //
+//
+//
+// 밑에서부터는 다시 디버깅
+//
+//
+// ** //
+// const API_CACHE = "api-cache";
+// const STATIC_RESOURCES = "static-resources";
+// const IMAGES_CACHE = "images";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+
+// if (workbox.navigationPreload.isSupported()) {
+//   workbox.navigationPreload.enable();
+// } // Adjust caching strategy for API requests
+// workbox.routing.registerRoute(
+//   new RegExp(`${SERVER_URI}/.*`), // Adjust the regex to match your API requests
+//   new workbox.strategies.NetworkFirst({
+//     cacheName: API_CACHE,
+//     plugins: [
+//       new workbox.expiration.ExpirationPlugin({
+//         maxEntries: 50,
+//         maxAgeSeconds: 24 * 60 * 60, // 1 day
+//       }),
+//     ],
+//     fetchOptions: {
+//       cache: "no-store",
+//     },
+//   }),
+// );
+
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches
+//       .open(CACHE)
+//       .then((cache) => cache.add(offlineFallbackPage))
+//       .then(() => {
+//         return self.skipWaiting();
+//       }),
+//   );
+// });
+
+// self.addEventListener("activate", (event) => {
+//   const cacheWhitelist = [CACHE, API_CACHE, STATIC_RESOURCES, IMAGES_CACHE];
+//   event.waitUntil(
+//     caches
+//       .keys()
+//       .then((cacheNames) => {
+//         return Promise.all(
+//           cacheNames.map((cacheName) => {
+//             if (!cacheWhitelist.includes(cacheName)) {
+//               return caches.delete(cacheName);
+//             }
+//           }),
+//         );
+//       })
+//       .then(() => self.clients.claim()),
+//   );
+// });
+
+// self.addEventListener("install", async (event) => {
+//   event.waitUntil(caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage)));
+// });
+
+// if (workbox.navigationPreload.isSupported()) {
+//   workbox.navigationPreload.enable();
+// }
+
+// workbox.routing.registerRoute(
+//   new RegExp("/_next/static/.*"),
+//   new workbox.strategies.StaleWhileRevalidate({
+//     cacheName: STATIC_RESOURCES,
+//   }),
+// ); // Adjust caching strategy for images
+// workbox.routing.registerRoute(
+//   new RegExp("/_next/image.*"),
+//   new workbox.strategies.StaleWhileRevalidate({
+//     cacheName: IMAGES_CACHE,
+//   }),
+// );
+
+// Fallback for navigation requests
+// self.addEventListener("fetch", (event) => {
+//   if (event.request.mode === "navigate") {
+//     event.respondWith(
+//       (async () => {
+//         try {
+//           const preloadResp = await event.preloadResponse;
+//           if (preloadResp) {
+//             return preloadResp;
+//           }
+
+//           const networkResp = await fetch(event.request);
+//           return networkResp;
+//         } catch (error) {
+//           const cache = await caches.open(CACHE);
+//           const cachedResp = await cache.match(offlineFallbackPage);
+//           return cachedResp;
+//         }
+//       })(),
+//     );
+//   }
+// });
