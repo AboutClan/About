e30eb7f1a9c33d89d15073ca72d54801ef1bc523
LSJ <j44s11@naver.com>
2024-07-06 22:08:47 +0900

    add image

--- storage/avatarStorage.ts
+++ storage/avatarStorage.ts
@@ -1,4 +1,4 @@
-export const AVATAR_COST = [0, 10, 30, 50, 70, 90, 110, 130, 150, 170, 190, 210];
+export const AVATAR_COST = [0, 10, 30, 50, 70, 90, 110, 130, 150, 170, 190, 210, 300];
 
 export const ABOUT_ICON =
   "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EB%8F%99%EC%95%84%EB%A6%AC/%EB%8F%99%EC%95%84%EB%A6%AC+%EC%95%84%EB%B0%94%ED%83%80.webp";
@@ -17,4 +17,5 @@ export const AVATAR_IMAGE_ARR = [
   "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%95%84%EB%B0%94%ED%83%80+%EC%95%84%EC%9D%B4%EC%BD%98/%ED%96%84%EC%8A%A4%ED%84%B0+2.webp",
   "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%95%84%EB%B0%94%ED%83%80+%EC%95%84%EC%9D%B4%EC%BD%98/%EC%83%81%EC%96%B4.webp",
   "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%95%84%EB%B0%94%ED%83%80+%EC%95%84%EC%9D%B4%EC%BD%98/%EC%83%81%EC%96%B4.webp",
+  "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EB%8F%99%EC%95%84%EB%A6%AC/%EB%85%B8%ED%8A%B8%EB%B6%81_100px_%ED%88%AC%EB%AA%85.png",
 ];
