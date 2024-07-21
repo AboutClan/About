3863a5cd09d2d2ec35c070b22d453bbddba71d30
LSJ <j44s11@naver.com>
2024-07-22 02:42:08 +0900

    wip

---
+++ pages/admin/test.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@chakra-ui/react";
+
+interface TestProps {}
+
+function Test({}: TestProps) {
+
+    
+  return (
+    <>
+      <Button>제출</Button>
+    </>
+  );
+}
+
+export default Test;
