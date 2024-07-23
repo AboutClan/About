6acd0e0cd16190fd66eca8525087684ee01e407c
createhb21 <alsghk9701@gmail.com>
2024-07-23 14:24:05 +0900

    default axios -> requestServer mathod

--- hooks/fcm/apis.ts
+++ hooks/fcm/apis.ts
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 import { SERVER_URI } from "../../constants/system";
+import { requestServer } from "../../libs/methodHelpers";
 import { DeviceInfo } from "./types";
 
 export const registerPushServiceWithApp = async (deviceInfo: DeviceInfo) => {
   try {
-    return await axios.post(`${SERVER_URI}/fcm/register-token`, deviceInfo);
+    return await requestServer({
+      method: "post",
+      url: "fcm/register-token",
+      body: deviceInfo,
+    });
   } catch (error) {
     console.error("Failed to subscribe push service on app:", error);
   }
