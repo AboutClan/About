3e106ce4c8e4f2a76c29c7baa59989469821e5a6
LSJ <j44s11@naver.com>
2024-07-24 01:27:27 +0900

    test components

--- pages/admin/test.tsx
+++ pages/admin/test.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@chakra-ui/react";
+import WritingNavigation from "../../components/atoms/WritingNavigation";
 
 function Test() {
   return (
     <>
       <Button>제출</Button>
+      <WritingNavigation />
     </>
   );
 }
