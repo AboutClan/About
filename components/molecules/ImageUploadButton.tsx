167cf3a370845d0cf12fcab01b6bd7bc8ca4e73d
LSJ <j44s11@naver.com>
2024-07-31 00:46:59 +0900

    add image condition

--- components/molecules/ImageUploadButton.tsx
+++ components/molecules/ImageUploadButton.tsx
@@ -1,6 +1,7 @@
 import { Button, Input } from "@chakra-ui/react";
 import { captureException } from "@sentry/nextjs";
 import { useRef } from "react";
+import { useToast } from "../../hooks/custom/CustomToast";
 
 import { DispatchType } from "../../types/hooks/reactTypes";
 import { processFile } from "../../utils/imageUtils";
@@ -12,11 +13,17 @@ interface IImageUploadButton {
 
 export default function ImageUploadButton({ setImageUrls, setImageForms }: IImageUploadButton) {
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const toast = useToast();
 
   const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = Array.from(e.target.files as FileList);
     const newImageUrls: string[] = [];
     const newImageForms: Blob[] = [];
+
+    if (files.length + newImageUrls.length > 5) {
+      toast("warning", "이미지는 최대 5장까지 가능합니다.");
+      return;
+    }
 
     for (const file of files) {
       try {
