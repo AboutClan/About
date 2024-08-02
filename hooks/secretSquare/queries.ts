b966f1fe5339b529e30a48de44858fadc40bd896
Jeongjin Oh <rojay.developer@gmail.com>
2024-08-02 16:38:57 +0900

    feat: square detail API 연동

--- hooks/secretSquare/queries.ts
+++ hooks/secretSquare/queries.ts
@@ -6,6 +6,7 @@ import { QueryOptions } from "../../types/hooks/reactTypes";
 import type {
   SecretSquareCategory,
   SecretSquareCategoryWithAll,
+  SecretSquareItem,
   SecretSquareType,
 } from "../../types/models/square";
 
@@ -34,8 +35,141 @@ export const useSecretSquareListQuery = (
       if (category !== "전체") {
         searchParams.set("category", category);
       }
-      const res = await axios.get<SecretSquareListResponse>(`${SERVER_URI}/square`);
-      return res.data;
+      //   const res = await axios.get<SecretSquareListResponse>(
+      //   `http://localhost:3001/square?${searchParams.toString()}`,
+      // );
+      return { squareList };
+      // const res = await axios.get<SecretSquareListResponse>(`${SERVER_URI}/square`);
+      // return res.data;
     },
     options,
   );
+
+type SecretSquareDetailResponse = SecretSquareItem;
+
+export const useGetSquareDetailQuery = (
+  { squareId }: { squareId: string },
+  options?: QueryOptions<SecretSquareDetailResponse>,
+) =>
+  useQuery<SecretSquareDetailResponse, AxiosError, SecretSquareDetailResponse>(
+    ["secretSquare", { squareId }],
+    async () => {
+      //   const res = await axios.get<SecretSquareDetailResponse>(
+      //   `http://localhost:3001/square/${squareId}`,
+      // );
+      return detail;
+      // const res = await axios.get<SecretSquareDetailResponse>(`${SERVER_URI}/square/${squareId}`);
+      // return res.data;
+    },
+    options,
+  );
+
+// TODO remove mock data
+const squareList: SecretSquareListResponse["squareList"] = [
+  {
+    _id: "66a89681d03a0dcf5b8cb216",
+    category: "일상",
+    title: "test title",
+    content: "테스트 본문입니다. 테스트 본문입니다. 테스트 본문입니다. ",
+    type: "poll",
+    viewCount: 13,
+    createdAt: "2024-07-30T07:30:09.027Z",
+    thumbnail:
+      "https://images.unsplash.com/photo-1591154669695-5f2a8d20c089?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    likeCount: 0,
+    commentsCount: 3,
+  },
+  {
+    _id: "66a89cfab648df8f5472d84d",
+    category: "일상",
+    title: "test title",
+    content: "테스트 본문입니다. 테스트 본문입니다. 테스트 본문입니다. ",
+    type: "poll",
+    viewCount: 0,
+    createdAt: "2024-07-30T07:57:46.764Z",
+    thumbnail: "",
+    likeCount: 0,
+    commentsCount: 0,
+  },
+  {
+    _id: "66a8b9bc6240f90fe315da0d",
+    category: "일상",
+    title: "tt",
+    content: "테스트 본문입니다. 테스트 본문입니다. 테스트 본문입니다.",
+    type: "poll",
+    viewCount: 0,
+    createdAt: "2024-07-30T10:00:28.037Z",
+    thumbnail: "",
+    likeCount: 0,
+    commentsCount: 0,
+  },
+  {
+    _id: "66a8f07df28eefb5818cd49f",
+    category: "고민",
+    title: "ttt",
+    content: "테스트 본문입니다. 테스트 본문입니다. 테스트 본문입니다.",
+    type: "poll",
+    viewCount: 1,
+    createdAt: "2024-07-30T13:54:05.194Z",
+    thumbnail: "",
+    likeCount: 0,
+    commentsCount: 0,
+  },
+];
+
+const detail: SecretSquareItem = {
+  _id: "66a89681d03a0dcf5b8cb216",
+  category: "일상",
+  title: "test title",
+  content: "테스트 본문입니다. 테스트 본문입니다. 테스트 본문입니다. ",
+  type: "poll",
+  poll: {
+    canMultiple: true,
+    pollItems: [
+      {
+        _id: "66a89681d03a0dcf5b8cb217",
+        name: "test1",
+        count: 1,
+      },
+      {
+        _id: "66a89681d03a0dcf5b8cb218",
+        name: "test2",
+        count: 1,
+      },
+      {
+        _id: "66a89681d03a0dcf5b8cb219",
+        name: "test3",
+        count: 0,
+      },
+    ],
+  },
+  images: [
+    "https://images.unsplash.com/photo-1591154669695-5f2a8d20c089?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1591154669695-5f2a8d20c089?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1591154669695-5f2a8d20c089?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ],
+  viewCount: 13,
+  createdAt: "2024-07-30T07:30:09.027Z",
+  updatedAt: "2024-07-30T23:45:57.650Z",
+  likeCount: 0,
+  comments: [
+    {
+      _id: "66a89c87774db8d2e59f7e5e",
+      comment: "댓글 예시입니다.",
+      createdAt: "2024-07-30T07:55:51.848Z",
+      updatedAt: "2024-07-30T07:55:51.848Z",
+    },
+    {
+      _id: "66a89c8d774db8d2e59f7e63",
+      comment: "댓글 예시입니다.1",
+      createdAt: "2024-07-30T07:55:57.663Z",
+      updatedAt: "2024-07-30T07:55:57.663Z",
+    },
+    {
+      _id: "66a8ae32e86eb8c7b4afd3a2",
+      comment: "1",
+      createdAt: "2024-07-30T09:11:14.100Z",
+      updatedAt: "2024-07-30T09:11:14.100Z",
+    },
+  ],
+};
--- pages/square/[id].tsx
+++ pages/square/[id].tsx
@@ -1,24 +1,30 @@
 import { Box, Button, Flex, Text, VStack } from "@chakra-ui/react";
 import dayjs from "dayjs";
-import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 
 import Avatar from "../../components/atoms/Avatar";
 import Divider from "../../components/atoms/Divider";
 import Header from "../../components/layouts/Header";
 import Slide from "../../components/layouts/PageSlide";
+import { usePrevious } from "../../hooks/custom/usePrevious";
+import { useGetSquareDetailQuery } from "../../hooks/secretSquare/queries";
 import PollItem from "../../pageTemplates/square/SecretSquare/PollItem";
 import SecretSquareComments from "../../pageTemplates/square/SecretSquare/SecretSquareComments";
 import { AVATAR_IMAGE_ARR } from "../../storage/avatarStorage";
-import { SecretSquareItem } from "../../types/models/square";
 import { getDateDiff } from "../../utils/dateTimeUtils";
 
 function SecretSquareDetailPage() {
+  const router = useRouter();
+  const squareId = router.query.id as string;
   // TODO remove mock data
   // TODO API
+  const { data: session } = useSession();
   // PATCH poll
   // GET poll current status (staleTime infinity)
   // DELETE comment
-  // GET square detail (staleTime infinity)
+  const { data: squareDetail } = useGetSquareDetailQuery({ squareId }, { staleTime: Infinity });
 
   // poll logic (initial)
   // users check poll items (inactive 투표하기)
@@ -57,191 +63,141 @@ function SecretSquareDetailPage() {
     <>
       <Header title="" />
       <Slide>
-        <Flex px={4} py={4} direction="column" gap={2} as="section" bg="white">
-          <Box bg="gray.200" rounded="full" w="fit-content" px={2} py={1}>
-            # {detail.category}
-          </Box>
-          <Flex as="section" align="center" gap={4}>
-            <Avatar isLink={false} image={AVATAR_IMAGE_ARR[0]} size="md" />
-            <Flex direction="column">
-              <Text fontWeight={500}>익명</Text>
-              <Text color="GrayText">{getDateDiff(dayjs(detail.createdAt))}</Text>
-            </Flex>
-          </Flex>
-          <Box as="section">
-            <Text as="h1" fontSize="xl" fontWeight={700}>
-              {detail.title}
-            </Text>
-            <Text mt={2}>{detail.content}</Text>
-          </Box>
-          {detail.type === "poll" && (
-            <Box
-              p={4}
-              my={4}
-              sx={{
-                width: "100%",
-                borderRadius: "var(--rounded-lg)",
-                border: "var(--border-main)",
-                background: "white",
-              }}
-            >
-              <VStack as="ul" align="flex-start">
-                <Text fontWeight={600}>투표</Text>
-                {detail.poll.pollItems.map(({ _id, name }, index) => {
-                  return (
-                    <PollItem
-                      key={index}
-                      isChecked={poll.has(_id)}
-                      value={name}
-                      onChange={() => {
-                        const isChecked = poll.has(_id);
-                        if (detail.poll.canMultiple) {
-                          setPoll((prev) => {
-                            const cloned = new Map(prev);
-                            if (isChecked) cloned.delete(_id);
-                            else cloned.set(_id, name);
-                            return cloned;
-                          });
-                        } else {
-                          setPoll((prev) => {
-                            const cloned = new Map(prev);
-                            if (isChecked) {
-                              cloned.delete(_id);
-                            } else if (cloned.size !== 0) {
-                              // if already checking other poll item
-                              cloned.clear();
-                              cloned.set(_id, name);
+        {squareDetail && (
+          <>
+            <Flex px={4} py={4} direction="column" gap={2} as="section" bg="white">
+              <Box bg="gray.200" rounded="full" w="fit-content" px={2} py={1}>
+                # {squareDetail.category}
+              </Box>
+              <Flex as="section" align="center" gap={4}>
+                <Avatar isLink={false} image={AVATAR_IMAGE_ARR[0]} size="md" />
+                <Flex direction="column">
+                  <Text fontWeight={500}>익명</Text>
+                  <Text color="GrayText">{getDateDiff(dayjs(squareDetail.createdAt))}</Text>
+                </Flex>
+              </Flex>
+              <Box as="section">
+                <Text as="h1" fontSize="xl" fontWeight={700}>
+                  {squareDetail.title}
+                </Text>
+                <Text mt={2}>{squareDetail.content}</Text>
+              </Box>
+              {squareDetail.type === "poll" && (
+                <Box
+                  p={4}
+                  my={4}
+                  sx={{
+                    width: "100%",
+                    borderRadius: "var(--rounded-lg)",
+                    border: "var(--border-main)",
+                    background: "white",
+                  }}
+                >
+                  <VStack as="ul" align="flex-start">
+                    <Text fontWeight={600} display="flex" gap={1} align="center">
+                      <Box display="flex" alignItems="center">
+                        <i className="fa-regular fa-check-to-slot" />
+                      </Box>
+                      <span>투표</span>
+                    </Text>
+                    {squareDetail.poll.pollItems.map(({ _id, name }, index) => {
+                      return (
+                        <PollItem
+                          key={index}
+                          isChecked={poll.has(_id)}
+                          value={name}
+                          onChange={() => {
+                            const isChecked = poll.has(_id);
+                            if (squareDetail.poll.canMultiple) {
+                              setPoll((prev) => {
+                                const cloned = new Map(prev);
+                                if (isChecked) cloned.delete(_id);
+                                else cloned.set(_id, name);
+                                return cloned;
+                              });
                             } else {
-                              cloned.set(_id, name);
+                              setPoll((prev) => {
+                                const cloned = new Map(prev);
+                                if (isChecked) {
+                                  cloned.delete(_id);
+                                } else if (cloned.size !== 0) {
+                                  // if already checking other poll item
+                                  cloned.clear();
+                                  cloned.set(_id, name);
+                                } else {
+                                  cloned.set(_id, name);
+                                }
+                                return cloned;
+                              });
                             }
-                            return cloned;
-                          });
-                        }
-                      }}
-                    />
-                  );
-                })}
+                          }}
+                        />
+                      );
+                    })}
+                    <Button
+                      type="button"
+                      rounded="lg"
+                      w="100%"
+                      colorScheme="mintTheme"
+                      isActive={isDirtyPoll}
+                      onClick={handlePatchPoll}
+                    >
+                      투표하기
+                    </Button>
+                  </VStack>
+                  <VStack as="ul"></VStack>
+                </Box>
+              )}
+
+              <Flex color="GrayText" align="center" gap={1}>
+                <i className="fa-light fa-eye" />
+                <span>{squareDetail.viewCount}명이 봤어요</span>
+              </Flex>
+
+              <Flex justify="space-between">
                 <Button
                   type="button"
-                  rounded="lg"
-                  w="100%"
-                  colorScheme="mintTheme"
-                  isActive={isDirtyPoll}
-                  onClick={handlePatchPoll}
+                  px="2"
+                  py="1"
+                  maxW="fit-content"
+                  backgroundColor="white"
+                  border="var(--border-main)"
+                  rounded="full"
+                  color="GrayText"
+                  gap={1}
+                  fontWeight={400}
+                  size="sm"
+                  sx={{
+                    // hover state is NOT removed on mobile device. It's confused for user
+                    // see https://github.com/chakra-ui/chakra-ui/issues/6173
+                    _hover: {},
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                  onClick={() => {
+                    // TODO put or delete like
+                    console.log("put or delete like");
+                  }}
                 >
-                  투표하기
+                  <i className="fa-light fa-thumbs-up" />
+                  <span>공감하기</span>
                 </Button>
-              </VStack>
+                <Flex gap={1} align="center">
+                  <i className="fa-light fa-comment" />
+                  <span>{squareDetail.comments.length}</span>
+                </Flex>
+              </Flex>
+            </Flex>
+            <Divider />
+            {/* comments section */}
+            <Box px={4} as="section" bg="white">
+              <SecretSquareComments />
             </Box>
-          )}
-
-          <Flex color="GrayText" align="center" gap={1}>
-            <i className="fa-light fa-eye" />
-            <span>{detail.viewCount}명이 봤어요</span>
-          </Flex>
-
-          <Flex justify="space-between">
-            <Flex
-              as="button"
-              px="2"
-              py="1"
-              maxW="fit-content"
-              backgroundColor="white"
-              border="var(--border-main)"
-              rounded="full"
-              color="GrayText"
-              type="button"
-              gap={1}
-              align="center"
-              onClick={() => {
-                // TODO put or delete like
-                console.log("put or delete like");
-              }}
-            >
-              <i className="fa-light fa-thumbs-up" />
-              <span>공감하기</span>
-            </Flex>
-            <Flex gap={1} align="center">
-              <i className="fa-light fa-comment" />
-              <span>{detail.comments.length}</span>
-            </Flex>
-          </Flex>
-        </Flex>
-        <Divider />
-        {/* comments section */}
-        <Box px={4} as="section" bg="white">
-          <SecretSquareComments />
-        </Box>
+          </>
+        )}
       </Slide>
     </>
   );
 }
-function usePrevious<T>(value: T): T {
-  const ref = useRef<T>(value);
-
-  // execute whenever initial render and re-render
-  useEffect(() => {
-    ref.current = value;
-  });
-
-  return ref.current;
-}
-
-const detail: SecretSquareItem = {
-  _id: "66a89681d03a0dcf5b8cb216",
-  category: "일상",
-  title: "test title",
-  content: "테스트 본문입니다. 테스트 본문입니다. 테스트 본문입니다. ",
-  type: "poll",
-  poll: {
-    canMultiple: true,
-    pollItems: [
-      {
-        _id: "66a89681d03a0dcf5b8cb217",
-        name: "test1",
-        count: 1,
-      },
-      {
-        _id: "66a89681d03a0dcf5b8cb218",
-        name: "test2",
-        count: 1,
-      },
-      {
-        _id: "66a89681d03a0dcf5b8cb219",
-        name: "test3",
-        count: 0,
-      },
-    ],
-  },
-  images: [
-    "https://images.unsplash.com/photo-1591154669695-5f2a8d20c089?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1591154669695-5f2a8d20c089?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1591154669695-5f2a8d20c089?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  ],
-  viewCount: 13,
-  createdAt: "2024-07-30T07:30:09.027Z",
-  updatedAt: "2024-07-30T23:45:57.650Z",
-  likeCount: 0,
-  comments: [
-    {
-      _id: "66a89c87774db8d2e59f7e5e",
-      comment: "댓글 예시입니다.",
-      createdAt: "2024-07-30T07:55:51.848Z",
-      updatedAt: "2024-07-30T07:55:51.848Z",
-    },
-    {
-      _id: "66a89c8d774db8d2e59f7e63",
-      comment: "댓글 예시입니다.1",
-      createdAt: "2024-07-30T07:55:57.663Z",
-      updatedAt: "2024-07-30T07:55:57.663Z",
-    },
-    {
-      _id: "66a8ae32e86eb8c7b4afd3a2",
-      comment: "1",
-      createdAt: "2024-07-30T09:11:14.100Z",
-      updatedAt: "2024-07-30T09:11:14.100Z",
-    },
-  ],
-};
 
 export default SecretSquareDetailPage;
