d3d0a177f03a4a67737e6f42846d4f7775a803e3
LSJ <j44s11@naver.com>
2024-09-21 16:51:55 +0900

    fix error

--- libs/study/getStudyConfimCondition.ts
+++ libs/study/getStudyConfimCondition.ts
@@ -6,7 +6,7 @@ export const getStudyConfimCondition = (
   studyDateStatus: StudyDateStatus,
   studyStatus: StudyStatus,
 ) => {
-  if (studyDateStatus === "today" && studyStatus === "pending" && getHour() === 16) {
+  if (studyDateStatus === "today" && studyStatus === "pending" && getHour() === 23) {
     return true;
   }
   return false;
