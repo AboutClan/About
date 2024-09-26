2bb4197e58a443f3a4f349a16a1d672495df4549
createhb21 <alsghk9701@gmail.com>
2024-09-26 16:57:59 +0900

    test vercel deploy

--- constants/nativeCustomEvent.ts
+++ constants/nativeCustomEvent.ts
@@ -6,9 +6,5 @@ export const NATIVE_CUSTOM_EVENTS = {
   CALL_PHONE: "callPhone",
   SHARE: "share",
   FOREGROUND_FCM: "foregroundFcm",
-  KAKAO_LOGIN: "kakaoLogin",
-  APPLE_LOGIN: "appleLogin",
-  KAKAO_LOGIN_CALLBACK: "kakaoLoginCallback",
-  APPLE_LOGIN_CALLBACK: "appleLoginCallback",
   GET_DEVICE_INFO: "getDeviceInfo",
 } as const;
