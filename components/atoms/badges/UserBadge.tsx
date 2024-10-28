e2442cbf726b843e3dd17b394f0c5f374658ec28
LSJ <j44s11@naver.com>
2024-10-28 19:41:18 +0900

    adjust ui

--- components/atoms/badges/UserBadge.tsx
+++ components/atoms/badges/UserBadge.tsx
@@ -9,13 +9,14 @@ interface IUserBadge {
 
 export default function UserBadge({ score, uid }: IUserBadge) {
   const badge = getUserBadge(score, uid);
- 
+
   return (
     <Badge
       h="20px"
       variant="subtle"
       px={2}
       py={1}
+      lineHeight="12px"
       fontWeight="semibold"
       fontSize="9px"
       borderRadius="10px"
