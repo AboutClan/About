437a19aa2cadda0ce72f61b6d154f4a1e451d274
LSJ <j44s11@naver.com>
2024-11-02 02:15:07 +0900

    chore: fix info

--- pageTemplates/setting/userSetting/userSettingPopUp.tsx
+++ pageTemplates/setting/userSetting/userSettingPopUp.tsx
@@ -1,8 +1,8 @@
 import { Box, Button, Flex } from "@chakra-ui/react";
 import dayjs from "dayjs";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
 import BottomFlexDrawer from "../../../components/organisms/drawer/BottomFlexDrawer";
@@ -129,7 +129,7 @@ export default function UserSettingPopUp({ userInfo }: UserSettingPopUpProps) {
       return;
     } else if (
       !checkAndSetLocalStorage("preference", 3) &&
-      (!userInfo?.studyPreference?.place ||
+      (!userInfo?.studyPreference?.updatedAt ||
         dayjs().diff(dayjs(userInfo?.studyPreference?.updatedAt), "day") > 30)
     ) {
       setModalTypes((old) => [...old, "preference"]);
