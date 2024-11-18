5babb7abd0429a80cc23244d895af11482ef0d00
LSJ <j44s11@naver.com>
2024-11-18 18:32:12 +0900

    .

--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, Flex, Grid } from "@chakra-ui/react";
-import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRecoilState, useSetRecoilState } from "recoil";
 
