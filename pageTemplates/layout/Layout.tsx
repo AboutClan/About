f6ef0de8140a83c294793b0d1eb39b2c3ef3af31
LSJ <j44s11@naver.com>
2024-12-03 17:08:18 +0900

    .

--- pageTemplates/layout/Layout.tsx
+++ pageTemplates/layout/Layout.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable @next/next/no-before-interactive-script-outside-document */
 
 import axios from "axios";
+import { signOut, useSession } from "next-auth/react";
 import dynamic from "next/dynamic";
 import { usePathname } from "next/navigation";
 import { useRouter } from "next/router";
-import { signOut, useSession } from "next-auth/react";
 import { useEffect, useMemo, useState } from "react";
 
 import BottomNav from "../../components/BottomNav";
@@ -65,11 +65,7 @@ function Layout({ children }: ILayout) {
     if (status === "loading" || session === undefined) return;
     const role = session?.user.role;
     if (role === "newUser") {
-      router.push("/login");
-      toast(
-        "info",
-        "접속 권한이 없습니다. 다시 로그인 해주세요! 반복되는 경우 관리자에게 문의 부탁드립니다.",
-      );
+      router.push("/register/name");
       return;
     }
     if (role === "waiting") {
