b4f526b175745b17afe00a988b73b8be099deabb
beming-dev <mingfordev@gmail.com>
2025-01-06 16:58:15 +0900

    Update image tag

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /home/ubuntu/app
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 294951093594.dkr.ecr.ap-northeast-2.amazonaws.com/about
+docker pull 294951093594.dkr.ecr.ap-northeast-2.amazonaws.com/about/front:latest
+docker stop nest-app || true
+docker rm nest-app || true
+docker run -d --name my-app -p 80:3001 294951093594.dkr.ecr.ap-northeast-2.amazonaws.com/about/front:latest
