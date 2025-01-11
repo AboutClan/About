7c39878ce50a9de28b4aeac834d79529dbf3c0a2
beming-dev <mingfordev@gmail.com>
2025-01-11 18:54:06 +0900

    Update sudo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 set -e  # 스크립트가 오류 발생 시 즉시 종료
 
 sudo chmod -R 775 /home/ubuntu/frontend
-cd /home/ubuntu/frontend
+sudo cd /home/ubuntu/frontend
 
 # ECR 로그인
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 294951093594.dkr.ecr.ap-northeast-2.amazonaws.com/frontend
