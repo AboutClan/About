c72deca8777b6b1a83ab58eb793a02aa6728c0d7
LSJ <j44s11@naver.com>
2025-04-14 13:42:04 +0900

    .

---
+++ assets/images/avatarImages.ts
@@ -0,0 +1,170 @@
+export const AVATAR_IMAGES = [
+  {
+    idx: 0,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B0%95%EC%95%84%EC%A7%80_%EB%88%84%EB%A0%811.png",
+  },
+  {
+    idx: 1,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B0%95%EC%95%84%EC%A7%80_%EB%88%84%EB%A0%812.png",
+  },
+  {
+    idx: 2,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B0%95%EC%95%84%EC%A7%80_%EB%88%84%EB%A0%813.png",
+  },
+  {
+    idx: 3,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B0%95%EC%95%84%EC%A7%80_%EC%A0%90%EB%B0%951.png",
+  },
+  {
+    idx: 4,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B0%95%EC%95%84%EC%A7%80_%EC%A0%90%EB%B0%952.png",
+  },
+  {
+    idx: 5,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B0%95%EC%95%84%EC%A7%80_%EC%A0%90%EB%B0%953.png",
+  },
+  {
+    idx: 6,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B0%95%EC%95%84%EC%A7%80_%EC%B4%88%EC%BD%941.png",
+  },
+  {
+    idx: 7,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B0%95%EC%95%84%EC%A7%80_%EC%B4%88%EC%BD%942.png",
+  },
+  {
+    idx: 8,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B0%95%EC%95%84%EC%A7%80_%EC%B4%88%EC%BD%943.png",
+  },
+  {
+    idx: 9,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B0%95%EC%95%84%EC%A7%80_%ED%9D%B01.png",
+  },
+  {
+    idx: 10,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B0%95%EC%95%84%EC%A7%80_%ED%9D%B02.png",
+  },
+  {
+    idx: 11,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B0%95%EC%95%84%EC%A7%80_%ED%9D%B03.png",
+  },
+  {
+    idx: 12,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B3%A0%EC%96%91%EC%9D%B4_%EA%B9%9C1.png",
+  },
+  {
+    idx: 13,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B3%A0%EC%96%91%EC%9D%B4_%EA%B9%9C1.png",
+  },
+  {
+    idx: 14,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B3%A0%EC%96%91%EC%9D%B4_%EA%B9%9C3.png",
+  },
+  {
+    idx: 15,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B3%A0%EC%96%91%EC%9D%B4_%EB%85%B8%EB%9E%911.png",
+  },
+  {
+    idx: 16,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B3%A0%EC%96%91%EC%9D%B4_%EB%85%B8%EB%9E%912.png",
+  },
+  {
+    idx: 17,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B3%A0%EC%96%91%EC%9D%B4_%EB%85%B8%EB%9E%913.png",
+  },
+  {
+    idx: 18,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B3%A0%EC%96%91%EC%9D%B4_%ED%95%91%ED%81%AC1.png",
+  },
+  {
+    idx: 19,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B3%A0%EC%96%91%EC%9D%B4_%ED%95%91%ED%81%AC2.png",
+  },
+  {
+    idx: 20,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B3%A0%EC%96%91%EC%9D%B4_%ED%95%91%ED%81%AC3.png",
+  },
+  {
+    idx: 21,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B3%A0%EC%96%91%EC%9D%B4_%ED%9A%8C1.png",
+  },
+  {
+    idx: 22,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B3%A0%EC%96%91%EC%9D%B4_%ED%9A%8C2.png",
+  },
+  {
+    idx: 23,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EA%B3%A0%EC%96%91%EC%9D%B4_%ED%9A%8C3.png",
+  },
+  {
+    idx: 24,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EB%B3%91%EC%95%84%EB%A6%AC_%EA%B8%B0%EB%B3%B8.png",
+  },
+  {
+    idx: 25,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EB%B3%91%EC%95%84%EB%A6%AC_%EB%85%B8%ED%8A%B8%EB%B6%81.png",
+  },
+  {
+    idx: 26,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EB%B3%91%EC%95%84%EB%A6%AC_%EC%B1%85.png",
+  },
+  {
+    idx: 27,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EB%B3%91%EC%95%84%EB%A6%AC1.png",
+  },
+  {
+    idx: 28,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EB%B3%91%EC%95%84%EB%A6%AC2.png",
+  },
+  {
+    idx: 29,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EB%B3%91%EC%95%84%EB%A6%AC3.png",
+  },
+  { idx: 30, image: "" },
+  { idx: 31, image: "" },
+  { idx: 32, image: "" },
+  {
+    idx: 33,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EB%82%A8%ED%8E%AD%EA%B7%841.png",
+  },
+  {
+    idx: 34,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EB%82%A8%ED%8E%AD%EA%B7%842.png",
+  },
+  {
+    idx: 35,
+    image:
+      "https://studyabout.s3.ap-northeast-2.amazonaws.com/%EC%BA%90%EB%A6%AD%ED%84%B0/%EC%95%84%EB%B0%94%ED%83%80/%EB%82%A8%ED%8E%AD%EA%B7%843.png",
+  },
+];
