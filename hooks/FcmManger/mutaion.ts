99b54cb277abd2901b6419c9dd14aa3371bed86b
LSJ <j44s11@naver.com>
2025-05-29 22:04:01 +0900

    .

--- hooks/FcmManger/mutaion.ts
+++ hooks/FcmManger/mutaion.ts
@@ -4,7 +4,7 @@ import { isWebView } from "../../utils/appEnvUtils";
 import { urlBase64ToUint8Array } from "../../utils/convertUtils/convertBase64";
 import { nativeMethodUtils } from "../../utils/nativeMethodUtils";
 import { useToast } from "../custom/CustomToast";
-import { registerPushServiceWithPWA } from "./apis";
+import { registerPushServiceWithApp, registerPushServiceWithPWA } from "./apis";
 import { DeviceInfo } from "./types";
 import { requestNotificationPermission } from "./utils";
 
@@ -42,7 +42,11 @@ const waitForDeviceInfo = (uid?: string): Promise<DeviceInfo> => {
 
         const deviceInfo = data;
 
-        // await registerPushServiceWithApp({ uid, ...deviceInfo });
+        await registerPushServiceWithApp({
+          uid,
+          fcmToken: deviceInfo.fcmToken,
+          platform: "android",
+        });
 
         resolve(deviceInfo); // ✅ deviceInfo 반환 가능
         window.removeEventListener("message", handleDeviceInfo);
