0541c2641eed219d90d8047bf6104e08c1e99b4a
LSJ <j44s11@naver.com>
2025-08-26 14:04:01 +0900

    .

--- next.config.js
+++ next.config.js
@@ -49,7 +49,7 @@ const baseNextConfig = {
   },
   // output: "standalone",
   images: {
-    unoptimized: false,
+    unoptimized: true,
 
     // 2) 최신 포맷으로 자동 변환 (용량↓)
     formats: ["image/avif", "image/webp"],
