e66cf192f9a7757edd793ab2a4669205a814048f
LSJ <j44s11@naver.com>
2025-12-02 17:54:08 +0900

    .

--- libs/study/getStudyVoteIcon.ts
+++ libs/study/getStudyVoteIcon.ts
@@ -80,9 +80,9 @@ export const getPlaceBasicIcon = (
            }; margin-bottom:4px; text-align:center; line-height:12px; font-weight:600;font-size:10px;color:#424242; background:white; border:1px solid #eeeeee; border-radius:4px; height:20px; " >
     <div style="max-width:${
       isBig ? "72px" : "80px"
-    }; text-overflow:ellipsis; white-space:nowrap; overflow:hidden;"> ${text}</div> <span style="margin:0 2px; margin-bottom:2px;">${StarIcon()}</span> ${rating.toFixed(
-          1,
-        )}</div>`
+    }; text-overflow:ellipsis; white-space:nowrap; overflow:hidden;"> ${text}</div> <span style="margin:0 2px; margin-bottom:2px;">${StarIcon()}</span> ${(
+          rating || 3
+        ).toFixed(1)}</div>`
       : ``
   }
   <svg xmlns="http://www.w3.org/2000/svg" width="32" height="36" viewBox="0 0 32 36" fill="none">
