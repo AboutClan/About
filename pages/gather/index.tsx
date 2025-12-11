e1c9dfdfb85093ba8ec5e4d994f615c7e9bf46ba
LSJ <j44s11@naver.com>
2025-12-11 16:42:52 +0900

    .

--- pages/gather/index.tsx
+++ pages/gather/index.tsx
@@ -1,6 +1,6 @@
 import { Box, Flex, Switch, Text } from "@chakra-ui/react";
-import { useRouter, useSearchParams } from "next/navigation";
 import { useSession } from "next-auth/react";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useQueryClient } from "react-query";
 import { useSetRecoilState } from "recoil";
@@ -14,7 +14,6 @@ import TabNav, { ITabNavOptions } from "../../components/molecules/navs/TabNav";
 import { useToast } from "../../hooks/custom/CustomToast";
 import { useGatherRequestMutation } from "../../hooks/gather/mutations";
 import { ModalLayout } from "../../modals/Modals";
-import PageGuideModal from "../../modals/PageGuideModal";
 import GatherHeader from "../../pageTemplates/gather/GatherHeader";
 import GatherMain from "../../pageTemplates/gather/GatherMain";
 import GatherPick from "../../pageTemplates/gather/GatherPick";
@@ -22,7 +21,6 @@ import SquareLoungeSection from "../../pageTemplates/square/SquareLoungeSection"
 import { sharedGatherWritingState } from "../../recoils/sharedDataAtoms";
 import { transferGatherDataState } from "../../recoils/transferRecoils";
 import { IModal } from "../../types/components/modalTypes";
-import { checkAndSetLocalStorage } from "../../utils/storageUtils";
 
 function Gather() {
   const router = useRouter();
@@ -35,16 +33,16 @@ function Gather() {
 
   const setTrasnferGatherData = useSetRecoilState(transferGatherDataState);
   const setTransferGatherWriting = useSetRecoilState(sharedGatherWritingState);
-  const [isModal, setIsModal] = useState(false);
+  // const [isModal, setIsModal] = useState(false);
   const [tab, setTab] = useState<"번개" | "라운지" | "이런 번개 어때요?">("번개");
   const [isGatherPickModal, setIsGatherPickModal] = useState(false);
 
   useEffect(() => {
     setTrasnferGatherData(null);
     setTransferGatherWriting(null);
-    if (!checkAndSetLocalStorage("gatherGuidePopUp", 21)) {
-      setIsModal(true);
-    }
+    // if (!checkAndSetLocalStorage("gatherGuidePopUp", 21)) {
+    //   setIsModal(true);
+    // }
   }, []);
 
   useEffect(() => {
@@ -132,12 +130,12 @@ function Gather() {
       {!isGuest && tab === "이런 번개 어때요?" && (
         <WritingButton onClick={() => setIsGatherPickModal(true)} />
       )} */}
-      {isModal && (
+      {/* {isModal && (
         <PageGuideModal title="번개 가이드" footerOptions={{}} setIsModal={setIsModal}>
           다양한 번개 모임에 참여해 보세요! 금방 마감될지도 모른다구요? 개설시에는 최대{" "}
           <b>15,000원</b> 지원금 획득! 참여 승인제 및 다양한 기능이 있습니다!
         </PageGuideModal>
-      )}
+      )} */}
       {isGatherPickModal && <GatherPickModal setIsModal={setIsGatherPickModal} />}
     </>
   );
