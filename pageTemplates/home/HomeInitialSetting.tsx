46aa4654dae0e09e2ebad9307c602f07cbe7a8b7
LSJ <j44s11@naver.com>
2026-01-06 12:38:05 +0900

    .

--- pageTemplates/home/HomeInitialSetting.tsx
+++ pageTemplates/home/HomeInitialSetting.tsx
@@ -3,7 +3,6 @@ import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { createGlobalStyle } from "styled-components";
 
-import { useDeepLink } from "../../@natives/useDeepLink";
 import { useToast } from "../../hooks/custom/CustomToast";
 import { usePushServiceInitialize } from "../../hooks/FcmManger/mutaion";
 import { useUserInfoFieldMutation } from "../../hooks/user/mutations";
@@ -14,7 +13,7 @@ import { navigateExternalLink } from "../../utils/navigateUtils";
 
 function HomeInitialSetting() {
   const { data: session } = useSession();
-  useDeepLink();
+
   usePushServiceInitialize({
     uid: session?.user?.uid,
   });
