d2e5de926e94d66ca252ea7407ea2a74a8d6c6a2
LSJ <j44s11@naver.com>
2026-02-22 15:16:22 +0900

    .

--- pageTemplates/home/HomeInitialSetting.tsx
+++ pageTemplates/home/HomeInitialSetting.tsx
@@ -5,7 +5,6 @@ import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { createGlobalStyle } from "styled-components";
 
-import AppDownloadModal from "../../components/overlay/AppDownloadModal";
 import ForceUpdateModal from "../../components/overlay/UpdateModal";
 import { useToast } from "../../hooks/custom/CustomToast";
 import { usePushServiceInitialize } from "../../hooks/FcmManger/mutaion";
@@ -202,13 +201,13 @@ function HomeInitialSetting() {
     sessionStorage.setItem("dismiss_app_download_modal", "1");
     setIsWeb(false);
   };
-
+  console.log(isLegacyApp, closeModal, isWeb);
   return (
     <>
       {userInfo && !isGuest && !isLegacyApp && <UserSettingPopUp user={userInfo} />}
       <GlobalStyle />
       {isLegacyApp && isApp() && <ForceUpdateModal onClose={() => setIsLegacyApp(false)} />}
-      {isWeb && <AppDownloadModal onClose={closeModal} />}
+      {/* {isWeb && <AppDownloadModal onClose={closeModal} />} */}
       {/* <Joyride
         hideCloseButton={true}
         callback={handleJoyrideCallback}
