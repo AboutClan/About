10d8c79cb09f6f5cba734ab4df98de950a30f282
LSJ <j44s11@naver.com>
2026-05-13 14:35:37 +0900

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,28 @@
 import { GetServerSideProps } from "next";
 
+import CafeMapPage from "./cafe-map";
+
 function Index() {
-  return null;
+  return <CafeMapPage />;
 }
+
 export default Index;
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const host = req.headers.host;
+
+  const isCafeMapDomain = host === "xn--ob0b42knwutje.com" || host === "www.xn--ob0b42knwutje.com";
+
+  if (isCafeMapDomain) {
+    return {
+      props: {},
+    };
+  }
+
   return {
     redirect: {
       permanent: false,
-      destination: `/home`,
+      destination: "/home",
     },
     props: {},
   };
